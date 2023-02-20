750581e433cde9af854d847d4541c6b42a9e4528
JorgePeju <pejuco@gmail.com>
2023-02-20 07:34:49 +0100

    cuadradoxd

--- JS/main.js
+++ JS/main.js
@@ -5,76 +5,81 @@
 
 // const arrayviajes = [];
 
-
-
-// function nombrefuncion() {
-
-// }
-
-
-const urlBase = "imagenes";
-const fragment = document.createDocumentFragment();
-
 document.addEventListener("DOMContentLoaded", () => {
 
-    const pintarbanner = (/*donde,que*/) => {
-        const fotos = document.querySelector("#foto img.imgFluid")
-        // otro const foto= getElementById("foto") devuelve un html collection
 
-        const arrayfotos = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
+    pintarBanner();
+    pintarRecomendado();
+
+}) //LOAD
+
+
+
+const urlBase = "/Imagenes";
+ //const fragment = document.createDocumentFragment();
+
+
+    const pintarBanner = () => {
+        const fotos = document.querySelector("#fotobanner .banner")
+        console.log(fotos)
+        const arrayFotos = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
         const arrayAlts = ["foto1", "foto2", "foto3", "foto4"];
 
-        const indice = Math.floor(Math.random()*arrayfotos.length);
+        //const indice = Math.floor(Math.random()*arrayFotos.length);
 
-        fotos.src = `${urlBase}/banner/${arrayfotos[indice]}`
-        fotos.alt = arrayAlts[indice]
+        fotos.src = `${urlBase}/banner/${arrayFotos[0]}`
+        fotos.alt = arrayAlts[0]
 
     }
 
-    const pintarrecomendado = () => {
+    
+    const pintarRecomendado = () => {
 
-        const recomendados = document.querySelector("#recomendados"); 5
+        const recomendados = document.querySelector("#fotorecom");
 
         // array multidimensional
 
         const arrayCards = [
-            ["Viajes1.jpg", "viaje uno", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus harum, corrupti, doloremque officia inventore architecto odit error eveniet nisi asperiores doloribus magnam tempora consequatur itaque ipsum dolores fugit, laborum commodi"],
-            ["Viajes2.jpg", "viaje uno", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus harum, corrupti, doloremque officia inventore architecto odit error eveniet nisi asperiores doloribus magnam tempora consequatur itaque ipsum dolores fugit, laborum commodi"],
-            ["Viajes3.jpg", "viaje uno", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus harum, corrupti, doloremque officia inventore architecto odit error eveniet nisi asperiores doloribus magnam tempora consequatur itaque ipsum dolores fugit, laborum commodi"],
+            ["viajes-1.jpg", "viaje uno", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus harum, corrupti, doloremque officia inventore architecto odit error eveniet nisi asperiores doloribus magnam tempora consequatur itaque ipsum dolores fugit, laborum commodi"],
+            ["viajes-2.jpg", "viaje uno", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus harum, corrupti, doloremque officia inventore architecto odit error eveniet nisi asperiores doloribus magnam tempora consequatur itaque ipsum dolores fugit, laborum commodi"],
+            ["viajes-3.jpg", "viaje uno", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus harum, corrupti, doloremque officia inventore architecto odit error eveniet nisi asperiores doloribus magnam tempora consequatur itaque ipsum dolores fugit, laborum commodi"],
         ];
 
-        arrayCards.forEach((item/*item, index, array.length*/) => {
-
+        arrayCards.forEach((item) => {
+             // item, index, array.length
             const card = document.createElement("ARTICLE");
-            card.classList.add("/*clase css")
-            const cardimg = document.createElement("IMG");
-            cardimg.src = `${urlBase}/viajes/${item[0]}`
-            cardimg.alt = `${urlBase}/viajes/${item[1]}`
+            card.classList.add("#fotorecom")
+            const cardImg = document.createElement("IMG");
+            cardImg.src = `${urlBase}/viajes/${item[0]}`
+            cardImg.alt = `${urlBase}/viajes/${item[1]}`
 
-            cardimg.classList.add("img-fluid")
+            cardImg.classList.add("#fotorecom")
             const cardh3 = document.createElement("H2");
             cardh3.textContent = item[1];
             const cardp = document.createElement("P");
             cardp.textContent = item[2];
 
-            card.append(cardimg, cardh3, cardp);
+            card.append(cardImg, cardh3, cardp);
 
 
             recomendados.append(card);
 
-
-            fragment.append(card); // constante arriba
+           // fragment.append(card); // constante arriba
+           
 
         })
 
         recomendados.append(card);
+        const bloque = document.getElementById("bloque");
 
+        cardImg.addEventListener("click", () => {
+        bloque.style.display = "block";
+});
     }
 
     const pintardestinos = () => {
 
     }
 
-    pintarbanner()
 
-}) //LOAD
+    
