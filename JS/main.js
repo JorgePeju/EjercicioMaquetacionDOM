2ae612d1ed5db1e7c83c58e03684e2b0d923f1a3
JorgePeju <pejuco@gmail.com>
2023-02-20 20:06:40 +0100

    sin bloque aun

--- JS/main.js
+++ JS/main.js
@@ -10,6 +10,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
     pintarBanner();
     pintarRecomendado();
+// pintarDestinos();
+
+    const bloque = document.getElementById("bloque");
+
+    cardImg.addEventListener("click", () => {
+    bloque.style.display = "block";
+});
 
 }) //LOAD
 
@@ -70,11 +77,6 @@ const urlBase = "/Imagenes";
         })
 
         recomendados.append(card);
-        const bloque = document.getElementById("bloque");
-
-        cardImg.addEventListener("click", () => {
-        bloque.style.display = "block";
-});
     }
 
     const pintardestinos = () => {
@@ -82,4 +84,89 @@ const urlBase = "/Imagenes";
     }
 
 
+    // VARIABLES
+
     
+    const lista= document.querySelector ("#lista");
+    const boton= document.querySelector ("#boton");
+
+
+
+    const arrayFotos = [
+        {
+            id: "a1",
+            nombre: "nombre1"
+        },
+         {
+            id: "a2",
+            nombre: "nombre2"
+        },
+        {
+            id: "a3",
+            nombre: "nombre3"
+        },
+        {
+            id: "a4",
+            nombre: "nombre4"
+        }
+    
+    ];
+        // EVENTOS
+
+
+        const arrayProductos = JSON.parse(localStorage.getItem("arrayCompra")) || []
+        boton.addEventListener("click", ()=>{
+                // Lo ponemos e una función flecha por que es una funcion expresada
+            pintarEnLista()
+        })
+
+        //lista.addEventListener("click",(ev)=>{  // ev da la propiedad de target (ev.target) y se podría desesctructurar poniendo {target}
+        lista.addEventListener("click",({target})=>{ 
+           if(target.matches("li")){
+            const id=target.id
+            mostrarElemento()
+           }
+        })
+
+
+        //FUNCIONES
+    const pintarEnLista= ()=>{
+        arrayFotos.forEach(({id,nombre})=>{
+        lista.innerHTML+= `<li id=${id}>${nombre}</li>`
+    })
+            // += si no se sobreescribiría el último ID (osea el 4)
+        
+    };
+
+
+    const mostrarElemento =()=>{
+        alert(id)
+    }
+
+
+document.addEventListener("click",({target})=>{
+
+    if(eval.target.matches("botonAdd")){
+       // const elemento =ev.target.parentElement.children[0].id
+
+       const id=ev.target.getAttribute("data-id")
+        addLocal()
+    }
+})
+
+   
+const eliminarDelLocal =(id)=>{
+    const nuevoArray = arrayCompra.filter((item)=>item.id!=id)
+
+    // este nuevo array va al local storage
+    montarLocal(nuevoArray)
+ }
+
+const addLocal=()=>{
+
+
+}
+
+const montarLocal=(nuevoArray)=>{
+    localStorage.setItem("cosa",JSON.stringify(nuevoArray))
+}
